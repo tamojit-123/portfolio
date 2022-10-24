4c8dbc01d35190ee470d617dee25a9b957f2d06e
Restyled.io <commits@restyled.io>
2022-10-24 18:16:25 +0000

    Restyled by prettier

--- src/containers/skills/DevOpsImg.js
+++ src/containers/skills/DevOpsImg.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import devops from "../../assets/lottie/devops.json";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
@@ -6,6 +6,6 @@ import DisplayLottie from "../../components/displayLottie/DisplayLottie";
 export default class DevOpsImg extends Component {
   render() {
     const theme = this.props.theme;
-    return < DisplayLottie animationData = { devops } />;
+    return <DisplayLottie animationData={devops} />;
   }
 }
--- src/containers/skills/SkillSection.js
+++ src/containers/skills/SkillSection.js
@@ -1,14 +1,14 @@
 import "./Skills.css";
 
-import React, {Component} from "react";
-import {Fade} from "react-reveal";
+import React, { Component } from "react";
+import { Fade } from "react-reveal";
 
 import landingPerson from "../../assets/lottie/landingPerson";
 import Button from "../../components/button/Button";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
 import SoftwareSkill from "../../components/softwareSkills/SoftwareSkill";
-import {skills} from "../../portfolio";
+import { skills } from "../../portfolio";
 
 import CloudInfraImg from "./CloudInfraImg";
 import DataScienceImg from "./DataScienceImg";
@@ -18,21 +18,14 @@ import FullStackImg from "./FullStackImg";
 
 function GetSkillSvg(props) {
   if (props.fileName === "DataScienceImg")
-    return <DataScienceImg theme = {
-      props.theme
-    } />;
+    return <DataScienceImg theme={props.theme} />;
   else if (props.fileName === "FullStackImg")
     return <FullStackImg theme={props.theme} />;
   else if (props.fileName === "DevOpsImg")
-    return <DevOpsImg theme = {
-      props.theme
-    } />;
+    return <DevOpsImg theme={props.theme} />;
   else if (props.fileName === "CloudInfraImg")
     return <CloudInfraImg theme={props.theme} />;
-  return <
-      DesignImg theme =
-      { props.theme
-      } />;
+  return <DesignImg theme={props.theme} />;
 }
 
 class SkillSection extends Component {
@@ -46,23 +39,28 @@ class SkillSection extends Component {
               <Fade left duration={2000}>
                 <div className="skills-image-div">
                   <GetSkillSvg fileName={skill.fileName} theme={theme} />
-      </div>
+                </div>
               </Fade>
 
-      <div className = "skills-text-div"><Fade right duration = {1000}>
-      <h1 className = "skills-heading" style = {{ color: theme.text }}>{
-          skill.title}</h1>
+              <div className="skills-text-div">
+                <Fade right duration={1000}>
+                  <h1 className="skills-heading" style={{ color: theme.text }}>
+                    {skill.title}
+                  </h1>
                 </Fade>
-      <Fade right duration = {1500}>
-      <SoftwareSkill logos =
-       { skill.softwareSkills } />
+                <Fade right duration={1500}>
+                  <SoftwareSkill logos={skill.softwareSkills} />
                 </Fade>
-      <Fade right duration = {2000}><div> {skill.skills.map((skillSentence) => {
+                <Fade right duration={2000}>
+                  <div>
+                    {" "}
+                    {skill.skills.map((skillSentence) => {
                       return (
                         <p
-                      className = "subTitle skills-text"
+                          className="subTitle skills-text"
                           style={{
-        color: theme.secondaryText }}
+                            color: theme.secondaryText,
+                          }}
                         >
                           {skillSentence}
                         </p>
@@ -77,6 +75,6 @@ class SkillSection extends Component {
       </div>
     );
   }
-  }
+}
 
-  export default SkillSection;
+export default SkillSection;
